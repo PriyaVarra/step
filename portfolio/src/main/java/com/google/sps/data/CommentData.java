af1697384b1b1e55b2218fa5c1600b01c1ce24f3
Priya Varra <priyavarra13@gmail.com>
2020-05-29 19:26:07 -0700

    Add name and timestamp information for each comment

--- portfolio/src/main/java/com/google/sps/data/CommentData.java
+++ portfolio/src/main/java/com/google/sps/data/CommentData.java
@@ -1,15 +1,30 @@
 package com.google.sps.data;
 
+import java.util.Date;
+
 /** Class containing comment data. */
 public final class CommentData {
 
+  private final String name;
   private final String comment;
-
-  public CommentData(String comment) {
+  private final String timeStamp;
+  
+  public CommentData(String name, String comment, String timeStamp) {
+    this.name = name;
     this.comment = comment;
+    this.timeStamp = timeStamp;
+  }
+
+  public String getName() {
+    return name;
   }
 
   public String getComment() {
     return comment;
   }
+
+  public String getTimeStamp() {
+    return timeStamp;
+  }
+
 }

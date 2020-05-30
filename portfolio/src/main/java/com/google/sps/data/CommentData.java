673c63b6b5442019c6d2d10420b8e5bb49a347ee
Priya Varra <priyavarra13@gmail.com>
2020-05-29 17:12:43 -0700

    Create class that stores data about comments to be displayed on webpage

---
+++ portfolio/src/main/java/com/google/sps/data/CommentData.java
@@ -0,0 +1,15 @@
+package com.google.sps.data;
+
+/** Class containing comment data. */
+public final class CommentData {
+
+  private final String comment;
+
+  public CommentData(String comment) {
+    this.comment = comment;
+  }
+
+  public String getComment() {
+    return comment;
+  }
+}
